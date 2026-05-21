7f06ee1740f019b1bf10b8343658c1d118ede749
mr-porsche <ilyas.sayyed@hotmail.com>
2026-05-21 15:55:14 +0530

    Fix: Imported Globals

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
 
 export default tseslint.config(
   js.configs.recommended,
